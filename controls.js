b51ec382a829adae11cd538100e6d2eb620a1fc8
Harshavardhan060718 <harshayata18@gmail.com>
2026-02-28 09:14:45 +0530

    Manually handled car

--- controls.js
+++ controls.js
@@ -1,66 +1,20 @@
-// class Controls {
-//     constructor() {
-//         this.forward = false;
-//         this.left = false;
-//         this.right = false;
-//         this.reverse = false;
-//         this.addkeyboardListeners();
-
-//     }
-//     addkeyboardListeners() {
-//         document.onkeydown = (event) => {
-//             switch (event.key) {
-//                 case "ArrowLeft":
-//                     this.left = true;
-//                     break;
-//                 case "ArrowRight":
-//                     this.right = true;
-//                     break;
-//                 case "ArrowUp":
-//                     this.forward = true;
-//                     break
-//                 case "ArrowDown":
-//                     this.reverse = true;
-//                     break;
-
-//             }
-//         }
-//         document.onkeyup = (event) => {
-//             switch (event.key) {
-//                 case "ArrowLeft":
-//                     this.left = false;
-//                     break;
-//                 case "ArrowRight":
-//                     this.right = false;
-//                     break;
-//                 case "ArrowUp":
-//                     this.forward = false;
-//                     break
-//                 case "ArrowDown":
-//                     this.reverse = false;
-//                     break;
-
-//             }
-//             console.table(this);
-//         }
-//     }
-
-
-// }
-
-
-
-
-
 class Controls {
-    constructor() {
+    constructor(type) {
         this.forward = false;
         this.left = false;
         this.right = false;
         this.reverse = false;
 
-        this.addKeyboardListeners();
+        switch (type) {
+            case "KEYS":
+                this.addKeyboardListeners();
+                break;
+            case "DUMMY":
+                this.forward = true;
+                break;
+        }
     }
+
 
     addKeyboardListeners() {
         document.onkeydown = (event) => {
@@ -78,9 +32,7 @@ class Controls {
                     this.reverse = true;
                     break;
             }
-
-        };
-
+        }
         document.onkeyup = (event) => {
             switch (event.key) {
                 case "ArrowLeft":
@@ -96,9 +48,6 @@ class Controls {
                     this.reverse = false;
                     break;
             }
-
-
-
-        };
+        }
     }
 }
